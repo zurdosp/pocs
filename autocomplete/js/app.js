43f4a83c098be26b67ab19747982ddd2dda2663b
zurdosp <zurdosp@gmail.com>
2017-05-28 21:16:08 -0300

    modifications autocomplite with bradcast

--- autocomplete/js/app.js
+++ autocomplete/js/app.js
@@ -1,17 +1,35 @@
 angular.module('myApp', ['ngTagsInput'])
 .controller('MainCtrl', function($scope, $http) {
 	$scope.tags = [];
-	var list = ["Amsterdam","Washington","Sydney","Beijing","Cairo","cairo new", "joSe Paulo"];
-	$scope.loadTags = function(query) {
+	var list = 	{"cities":[{nome:"Amsterdam", sigla:"AMS"},{nome:"Peru", sigla:"PE"},{nome:"Beijing", sigla: "BEI"},{nome:"Cairo", sigla:"CAI"},{nome:"AREQUIPA", sigla:"AQP"}]}
+	$scope.loadTags = function(query, element) {
 		if (query != '' && query !=  null){
-			//return $http.get('/tags?query=' + query);
-			return list.filter(function(datalist) {
-				if (datalist.toLowerCase().indexOf(query.toLowerCase()) >= 0){
+			return list.cities.filter(function(datalist) {
+				if (datalist.nome.toLowerCase().indexOf(query.toLowerCase()) >= 0){
 					return datalist;
 				}
 			});
 		} else {
-			return list;
+			return list.cities;
 		}
 	};
+	
+	$scope.checkAddElement = function(tag, element){
+		if (element.tags.length == 0){
+			document.getElementById('nome').style.width = 70 + (tag.nome.length * 7) + 'px';			
+			return true;
+		} else {
+			return false;
+		}
+	};
+	
+	$scope.addElement = function() {
+		$scope.tags.push({ sigla: 'aaa', nome: 'cc'});
+	}
+
+	$scope.save = function(){
+		angular.forEach($scope.tags, function(tag){
+			//alert("nome: " + tag.nome + " , sigla: " + tag.sigla);
+		});
+	}
 });
