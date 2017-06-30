7f9a4e7d7195fdac848294e8ca8bb08cf141c9a1
zurdosp <zurdosp@gmail.com>
2017-06-30 20:25:45 -0300

    mais exemplos de jaxb com model

---
+++ JaxbBasicDemo/src/main/java/com/websystique/xml/model/University.java
@@ -0,0 +1,46 @@
+package com.websystique.xml.model;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+@XmlRootElement(name = "University")
+@XmlType(propOrder = { "name", "address", "students" })
+public class University {
+
+	List<Student> students;
+
+	private String name;
+
+	private String address;
+
+	@XmlElementWrapper(name = "Students")
+	@XmlElement(name = "Student")
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Student> students) {
+		this.students = students;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
