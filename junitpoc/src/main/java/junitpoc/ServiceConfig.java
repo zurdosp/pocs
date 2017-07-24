2a7f5aff9969d4f8092752370ed611e7467e1260
Christian Oscar Tejada Pacheco <christian.pacheco@rmansur.com.br>
2017-07-24 19:29:59 -0300

    adasdasd
    das

---
+++ junitpoc/src/main/java/junitpoc/ServiceConfig.java
@@ -0,0 +1,62 @@
+package junitpoc;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Component
+@PropertySources({ @PropertySource(value = "classpath:service.properties"),
+		@PropertySource(value = "classpath:database.properties") })
+public class ServiceConfig {
+
+	@Value("${server.port}")
+	private String servicePort;
+
+	@Value("${database1}")
+	private String database1;
+
+	public String getServicePort() {
+		return servicePort;
+	}
+
+	public void setServicePort(String servicePort) {
+		this.servicePort = servicePort;
+	}
+
+	@Bean
+	@Autowired
+	public PlatformTransactionManager transactionManager(DataSource dataSource) {
+		return new DataSourceTransactionManager(dataSource);
+	}
+
+	@Bean
+	public DataSource dataSource(@Value("${database.host}") String host, @Value("${database.port}") Integer port,
+			@Value("${database.username}") String username, @Value("${database.password}") String password,
+			@Value("${database.name}") String database, @Value("${database.instance.name}") String instanceName,
+			@Value("${database.pool}") Integer pool) {
+
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		dataSource.setUrl(host + "" + port);
+		dataSource.setUsername(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	public String getDatabase1() {
+		return database1;
+	}
+
+	public void setDatabase1(String database1) {
+		this.database1 = database1;
+	}
+
+}
---
+++ junitpoc/src/main/java/junitpoc/ServicesPoc.java
@@ -0,0 +1,33 @@
+package junitpoc;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import junitpoc.services.Service1;
+
+@RestController()
+public class ServicesPoc {
+
+	@Autowired
+	private Service1 service1;
+
+	@Autowired
+	private ServiceConfig serviceConfig;
+
+	@RequestMapping(value = "/students/{studentId}", method = GET)
+	public String retrieveCoursesForStudent(@PathVariable String studentId) {
+		System.out.println(serviceConfig.getDatabase1());
+		System.out.println(serviceConfig.getServicePort());
+
+		if (service1.getNomeFull().equalsIgnoreCase("christian tejada")) {
+			return serviceConfig.getDatabase1();
+		} else {
+			return "nao sou eu";
+		}
+	}
+
+}
