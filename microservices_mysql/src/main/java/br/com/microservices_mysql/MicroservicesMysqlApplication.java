c2c3defef5df0a6bdb2dd508ad193a9ed91de47a
zurdosp <zurdosp@gmail.com>
2017-07-27 07:45:32 -0300

    ddd

---
+++ microservices_mysql/src/main/java/br/com/microservices_mysql/MicroservicesMysqlApplication.java
@@ -0,0 +1,12 @@
+package br.com.microservices_mysql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesMysqlApplication.class, args);
+	}
+}
