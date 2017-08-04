55d0466fd41e1a9e60edf69b0ef34a971c09ceb7
zurdosp <zurdosp@gmail.com>
2017-08-03 22:39:18 -0300

    kafka 100%

---
+++ service-template-kafka-consumer2/src/main/java/junitpoc/ServicesPocConsumer2App.java
@@ -0,0 +1,13 @@
+package junitpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesPocConsumer2App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesPocConsumer2App.class, args);
+	}
+
+}
---
+++ service-template-kafka-consumer3/src/main/java/junitpoc/ServicesPocConsumer3App.java
@@ -0,0 +1,13 @@
+package junitpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicesPocConsumer3App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicesPocConsumer3App.class, args);
+	}
+
+}
